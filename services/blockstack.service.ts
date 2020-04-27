457151763b9c6f3dd8d3550a4ffa65f26776905a
joris hermans <JHermans85@hotmail.com>
2020-04-28 00:53:55 +0200

    newer version of blockstack provider

--- services/blockstack.service.ts
+++ services/blockstack.service.ts
@@ -9,15 +9,13 @@ export class BlockStackApiService {
     private user: {person: Person, username: string} | undefined = undefined;
     private userSession: UserSession;
     private signInNotify: Function[];
-    private blockStackProvider: BlockstackProvider;
 
     constructor() {
         const appConfig = new AppConfig(['store_write', 'publish_data'])
         this.userSession = new UserSession({ appConfig: appConfig });
         this.signInNotify = [];
 
-        this.blockStackProvider = new BlockstackProvider(this.userSession);
-        ss.registerProvider(this.blockStackProvider);
+        ss.registerProvider(new BlockstackProvider(this.userSession));
     }
 
     public getAuthOptions() {
@@ -34,8 +32,7 @@ export class BlockStackApiService {
           // console.log('finished', this.signInNotify, this.signInNotify.length);
           this.signInNotify.forEach(fn => {
             fn();
-          });
-          this.blockStackProvider.userSession = u.userSession;
+          })
         },
         appDetails: {
           name: 'Guidle Me',
